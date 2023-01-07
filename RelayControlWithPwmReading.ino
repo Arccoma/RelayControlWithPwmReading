e0e190a286a7f026b985182067c31e92456e0891
Arccoma <mass0@naver.com>
2023-01-07 17:43:45 +0900

    PWM table modify: from 12 to 64

--- RelayControlWithPwmReading.ino
+++ RelayControlWithPwmReading.ino
@@ -2,37 +2,83 @@
  Relay Control with PWM reading value
  Relay Module: LK 임베디드 6channel HL-56S (Low Level Trigger)
  */
-#define PWM_R_RESET     1000
-#define PWM_L_RESET     1000
 
-#define PWM_LEFT_1    1175
-#define PWM_LEFT_2    1250
-#define PWM_LEFT_3    1325
-#define PWM_LEFT_4    1400
-#define PWM_LEFT_5    1475
-#define PWM_LEFT_ALL  1100
+//--------Left Side PWM VALUE
+#define PWM_L_00000   1000
+#define PWM_L_00001   1015
+#define PWM_L_00010   1030
+#define PWM_L_00011   1045
+#define PWM_L_00100   1060
+#define PWM_L_00101   1075
+#define PWM_L_00110   1090
+#define PWM_L_00111   1105
+#define PWM_L_01000   1120
+#define PWM_L_01001   1135
 
-#define PWM_RIGHT_1   1625
-#define PWM_RIGHT_2   1700
-#define PWM_RIGHT_3   1775
-#define PWM_RIGHT_4   1850
-#define PWM_RIGHT_5   1925
-#define PWM_RIGHT_ALL 1550
-/*
-#define PWM_LEFT_1    1100//1175
-#define PWM_LEFT_2    1175//1250
-#define PWM_LEFT_3    1250//1325
-#define PWM_LEFT_4    1325//1400
-#define PWM_LEFT_5    1400//1475
-#define PWM_LEFT_ALL  1475//1100
+#define PWM_L_01010   1150
+#define PWM_L_01011   1165
+#define PWM_L_01100   1180
+#define PWM_L_01101   1195
+#define PWM_L_01110   1210
+#define PWM_L_01111   1225
+#define PWM_L_10000   1240
+#define PWM_L_10001   1255
+#define PWM_L_10010   1270
+#define PWM_L_10011   1285
 
-#define PWM_RIGHT_1   1550//1625
-#define PWM_RIGHT_2   1625//1700
-#define PWM_RIGHT_3   1700//1775
-#define PWM_RIGHT_4   1775//1850
-#define PWM_RIGHT_5   1850//1925
-#define PWM_RIGHT_ALL 1925//1550
-*/
+#define PWM_L_10100   1300
+#define PWM_L_10101   1315
+#define PWM_L_10110   1330
+#define PWM_L_10111   1345
+#define PWM_L_11000   1360
+#define PWM_L_11001   1375
+#define PWM_L_11010   1390
+#define PWM_L_11011   1405
+#define PWM_L_11100   1420
+#define PWM_L_11101   1435
+
+#define PWM_L_11110   1450
+#define PWM_L_11111   1465
+
+//--------Right Side PWM VALUE
+#define PWM_R_00000   1000+500
+#define PWM_R_00001   1015+500
+#define PWM_R_00010   1030+500
+#define PWM_R_00011   1045+500
+#define PWM_R_00100   1060+500
+#define PWM_R_00101   1075+500
+#define PWM_R_00110   1090+500
+#define PWM_R_00111   1105+500
+#define PWM_R_01000   1120+500
+#define PWM_R_01001   1135+500
+
+#define PWM_R_01010   1150+500
+#define PWM_R_01011   1165+500
+#define PWM_R_01100   1180+500
+#define PWM_R_01101   1195+500
+#define PWM_R_01110   1210+500
+#define PWM_R_01111   1225+500
+#define PWM_R_10000   1240+500
+#define PWM_R_10001   1255+500
+#define PWM_R_10010   1270+500
+#define PWM_R_10011   1285+500
+
+#define PWM_R_10100   1300+500
+#define PWM_R_10101   1315+500
+#define PWM_R_10110   1330+500
+#define PWM_R_10111   1345+500
+#define PWM_R_11000   1360+500
+#define PWM_R_11001   1375+500
+#define PWM_R_11010   1390+500
+#define PWM_R_11011   1405+500
+#define PWM_R_11100   1420+500
+#define PWM_R_11101   1435+500
+
+#define PWM_R_11110   1450+500
+#define PWM_R_11111   1465+500
+
+
+
 
 const int PWM_INPUT_PIN = A0; // MIN 978 ~ MAX 1990 ( MIDDLE 1500 )
 const int relay1 = 12; 
@@ -89,7 +135,30 @@ void loop() {
   PWM_in = pulseIn(PWM_INPUT_PIN, HIGH);
   Serial.println(PWM_in);
 
+  if ( ( (PWM_L_00000-ADJUSTMENT_RANGE) < PWM_in ) &&
+       (PWM_L_00000 >= PWM_in)                          )
+  {
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
+    digitalWrite(relay4, HIGH);
+    digitalWrite(relay5, HIGH);     
+  }
 
+  if ( ( (PWM_L_00001-ADJUSTMENT_RANGE) < PWM_in ) &&
+       (PWM_L_00001 >= PWM_in)                        )
+  {
+    digitalWrite(relay5, LOW);  
+  }
+  
+  if ( ( (PWM_L_00010-ADJUSTMENT_RANGE) < PWM_in ) &&
+       (PWM_L_00010 >= PWM_in)                        )
+  {
+    digitalWrite(relay4, LOW);  
+  }
+
+
+/*
 if( !flgL_1 && 
       ( (PWM_LEFT_1-ADJUSTMENT_RANGE) < PWM_in ) &&  
       ( (PWM_LEFT_1+ADJUSTMENT_RANGE) > PWM_in ) )
@@ -269,4 +338,5 @@ if( !flgL_1 &&
     flgR_All = 0;
     flgR_Reset = 1;
   }
+  */
 }
